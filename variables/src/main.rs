5a883219b50f03f71c3c506a3b7070a81f1a2281
dreambo8563 <dreambo8563@outlook.com>
2017-06-29 22:17:11 +0800

    What : module example communicator
    Why:
    
    * practise module structure
    
    How:
    
    * communicator

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,8 +3,8 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
-    
- //   const MAX_POINTS: u32 = 100_000;
+
+    //   const MAX_POINTS: u32 = 100_000;
     // let x = 5;
 
     let x = x + 1;
@@ -12,4 +12,27 @@ fn main() {
     let x = x * 2;
 
     println!("The value of x is: {}", x);
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+
+    // remainder
+    let remainder = 43 % 5;
+
+    println!(
+        "{},{},{},{},{}",
+        sum,
+        difference,
+        product,
+        quotient,
+        remainder
+    );
 }
